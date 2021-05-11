c0395c9540122c7a66fcdbce7e64dbe7639385e9
En8oy PC Master <irbing.moreno@outlook.com>
2021-05-11 16:46:13 -0500

    Correction

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class)->only("index", "show");
 Route::resource('users', UserController::class);
 
 

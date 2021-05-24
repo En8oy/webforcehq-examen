8cc5994a6ea82b736e3aaedfa290d17799a5492f
Irbing Alejandro Moreno Garcia <31268601+En8oy@users.noreply.github.com>
2021-05-24 14:32:13 -0500

    Update About

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,50 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h1 v-for="city in cities" :key="city.name" v-text="city.name"></h1> -->
+    <v-autocomplete :items="cities" item-text="name" item-value="alpha3Code" v-model="city">
+
+    </v-autocomplete>
+    <v-btn :loading="loading">
+      Hola
+    </v-btn>
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      cities : [],
+      city : "",
+      loading : false
+    }
+  },
+  methods: {
+    getCities(){
+      axios.get("https://restcountries.eu/rest/v2/all")
+      .then(res => {
+        this.cities = res.data
+        this.loading = false
+        this.$toast.open({
+          position: "top-right",
+          message: "Datos obtenidos correctamente",
+          type: "success",
+        });
+      })
+      .catch(err => {
+        this.loading = false
+
+         this.$toast.open({
+          position: "top-right",
+          message: "Hubo un error en la consulta de datos",
+          type: "error",
+        });
+      })
+    }
+  },
+  mounted() {
+    this.loading = true
+    this.getCities()
+  },
+}
+</script>
